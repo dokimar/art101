4fa85828f801ad9f9edcc29e2018d7ca44575de8
dokimar <chudoki.ml@gmail.com>
2021-10-13 22:06:24 -0700

    Lab 4 update

---
+++ lab4/js/lab.js
@@ -0,0 +1,21 @@
+/* UCSC; ART101 Fall 2021; Lab 4: Pseudocoding and Problem-solving.
+ * Pseudocode an everyday task and a potential computer task.
+ * @author  Ariana Lazich and Marlene Lopez
+ * @since   14 October 2021
+*/
+
+/* Pseudocode a computer task
+1. highlevel/overarching tasks for computer program
+2. keep it high level
+3. break down complicated tasks into sub-tasks if necessary
+4. copy paste/include outline in submission doc
+*/
+
+//Read grade categories (e.g. final, project, quizzes) and base
+//Accept user input of student actual grades
+  //Calculate final, final max, final weight
+  //Calculate project, project max, project weight
+  //Calculate Quiz, Quiz Max, Quiz weight
+  //Process category totals
+//Print total grade (addition of category totals)
+//If total grade > 75, mark “Pass”
