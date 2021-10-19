5a66dc31a3e4bef17ee0c09374dfa4c9ce06148e
dokimar <chudoki.ml@gmail.com>
2021-10-18 20:23:16 -0700

    lab 5 upload

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/* UCSC; ART101 Fall 2021; Lab 5: Data Types and Variables.
+ * Experiment with basic JavaScript data types and variables.
+ * @author  Ariana Lazich and Marlene Lopez <mlope109@ucsc.edu>
+ * @since   19 October 2021
+*/
+
+// Define variables
+make = "Honda";
+model = "Civic";
+color = "red";
+year = 2012;
+
+// Calculate
+age = 2021 - year;
+
+// Output: print statement is WRITE*L*N, not uppercase 'i'
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+document.writeln("Owned: " + ownIt + "<br>");
