dbaf3df5d6ec77bae15b891e58c1173849cb11a6
dokimar <chudoki.ml@gmail.com>
2021-10-20 21:30:15 -0700

    upload lab6 lab.js

---
+++ lab6/js/lab.js
@@ -0,0 +1,37 @@
+/* UCSC; ART101 Fall 2021; Lab 6: Arrays and Objects.
+ * Experiment with basic JavaScript arrays and objects.
+ * Author(s):   Ariana Lazich and Marlene Lopez <mlope109@ucsc.edu>
+ * Created:     21 October 2021
+*/
+
+
+// an array myTransport that lists all of the forms of transportation you use to get around
+var myTransport = ["car", "bus", "train", "bike", "scooter"];
+
+
+// an object myMainRide that collects all the data you specified for your primary vehicle in Lab 5
+var myMainRide = {
+  type: myTransport[0],
+  make: 'Honda',
+  model: 'Civic',
+  color: 'red',
+  year: 2012
+};
+
+
+// add age to myMainRide object
+myMainRide.age = 2021 - myMainRide.year;
+
+
+// Output
+  // Use document.writeln() to neatly output myTransport
+  document.writeln("Getting around: " +
+                    myTransport[0] + ", " +
+                    myTransport[1] + ", " +
+                    myTransport[2] + ", " +
+                    myTransport[3] + ", " +
+                    myTransport[4] + "<br>"
+                  );
+  // Use document.writeln() to neatly output myMainRide
+  document.writeln("<p>My Main Ride: <pre>",
+                    JSON.stringify(myMainRIde, null, '\t'), ""</pre></p>");
